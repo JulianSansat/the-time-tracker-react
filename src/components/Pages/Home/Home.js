5c0278fcb1c1aaac487e47a56ea2e2874425ec4f
Julian <juliansansat@gmail.com>
2019-04-03 13:20:04 -0300

    changed filters

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -10,20 +10,25 @@ class Home extends Component {
         this.state = {
             logs: [],
             errorWarning: 'hide',
-            selectedUser: null,
-            logFilters: []
+            selectedUser: '',
+            year: '',
+            month: '',
+            day: '',
+            days: [],
+            logFilters: [],
+            maxYearsFilter: 3
         }
     }
 
     componentDidMount() {
         Api.get(`/logs`)
         .then(res => {
-        if(res.status === 200){
-          this.setState({
-            'errorWarning': 'hide',
-            'logs': res.data.data
-          });
-        }
+          if(res.status === 200){
+            this.setState({
+              'errorWarning': 'hide',
+              'logs': res.data.data
+            });
+          }
         })
         .catch(error => {
           if(error.response != undefined){
@@ -46,6 +51,54 @@ class Home extends Component {
     handleUserSelectChange = (selectedUser) => {
       this.setState({ selectedUser });
       this.filterLogs('user_id', selectedUser.user_id);
+    }
+
+    handleDateSelectChange = (e) => {
+      this.setState({[e.target.name]: e.target.value});
+      if(e.target.name === 'month' && e.target.value !== ''){
+        let month = moment().month(e.target.value).format("MM");
+        let numberOfDays = moment(this.state.year+'-'+month, "YYYY-MM").daysInMonth()
+        let days = [];
+        for (var i = 1; i <= numberOfDays; i++) {
+            days.push(i);
+        }
+        this.setState({
+          days: days
+        })
+      }
+      if(e.target.name === 'month' && e.target.value === ''){
+        this.setState({
+          days: []
+        })
+      }
+      let date = '2018-01-01 00:00:00';
+      let year = this.state.year;
+      let month = this.state.month !== '' ? moment().month(this.state.month).format("MM") : '01';
+      let day = this.state.day !== '' ? this.state.day : '01';
+
+      if(e.target.name === 'year'){
+        year = e.target.value;
+        this.filterLogs('year', year);
+      }
+
+      if(e.target.name === 'month'){
+        month = e.target.value !== '' ? moment().month(e.target.value).format("M") : '';
+        this.filterLogs('month', month);
+      }
+
+      if(e.target.name === 'day'){
+        day = e.target.value !== '' ? moment().day(e.target.value).format("DD") : '';
+        this.filterLogs('day', day);
+      }
+
+      date = year+'-'+month+'-'+day+' 00:00:00';
+
+
+      if(e.target.name === 'date'){
+        if(moment(date).isValid()){
+          this.filterLogs('date', date);
+        }
+      }
     }
 
     filterLogs = (key, value) => {
@@ -86,6 +139,14 @@ class Home extends Component {
     }
 
     render() {
+
+        const lastYears = [];
+        for (var i = 0; i < this.state.maxYearsFilter; i++) {
+            lastYears.push(moment().subtract(i, 'y').format('YYYY'));
+        }
+
+        const months = moment.months();
+
         const promiseOptions = inputValue => {
           return Api.get(`/users`, {params:{first_name: inputValue}}).then(res => {
             return res.data.data.map(obj => {
@@ -96,13 +157,69 @@ class Home extends Component {
 
         return (
             <section className="section">
-                <AsyncSelect
-                  cacheOptions={true}
-                  value={this.state.selectedUser}
-                  onChange={this.handleUserSelectChange}
-                  defaultOptions={true}
-                  loadOptions={promiseOptions}
-                />
+                <div className="columns">
+                  <div className="column">
+                    <div class="field">
+                      <label class="label" for="userSelect">User</label>
+                      <div class="control">
+                        <AsyncSelect
+                          cacheOptions={true}
+                          value={this.state.selectedUser}
+                          onChange={this.handleUserSelectChange}
+                          defaultOptions={true}
+                          loadOptions={promiseOptions}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div class="field">
+                      <label class="label" for="year">Year</label>
+                      <div class="control">
+                        <div className="select">
+                          <select name="year" onChange={this.handleDateSelectChange}>
+                            <option value=''></option>
+                            {lastYears.map((year) => 
+                              <option value={year}>{year}</option>
+                            )}
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div class="field">
+                      <label class="label" for="month">Month</label>
+                      <div class="control">
+                        <div className="select">
+                          <select name="month" onChange={this.handleDateSelectChange}>
+                            <option value=''></option>
+                            {months.map((month) => 
+                              <option value={month}>{month}</option>
+                            )}
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="column">
+                    <div class="field">
+                      <label class="label" for="day">Day</label>
+                      <div class="control">
+                        <div className="select">
+                          <select name="day" onChange={this.handleDateSelectChange}>
+                            <option value=''></option>
+                            {this.state.days.map((day) => 
+                              <option value={day}>{day}</option>
+                            )}
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                
+                
                 <div className="box">
                     <article class={"message is-danger " + this.state.errorWarning}>
                       <div class="message-header">
